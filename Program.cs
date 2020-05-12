56bd46d0a1516c93859868cb595aa06ec65085c9
Decimus <winendou@gmail.com>
2020-05-12 17:00:21 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { };
+            while (true)
+            { Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            names.Add(name);
+            Console.WriteLine("нажмите 1 - чтобы продолжить, 2 - чтобы выйти ");
+            if (Console.ReadLine() == "2")
+            { break; }
+            }
+            names.Sort();
+            names = names.Distinct().ToList();
+            foreach (string name in names)
+            { Console.WriteLine(name); }
+            Console.WriteLine("Всего имен" + names.Count);
+            Console.ReadLine();
+        }
+    }
+}
